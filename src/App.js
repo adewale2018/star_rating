74c3a39732717cbe8370a24e3dab05a7f2dabb1a
adewale2018 <saheedadewaleshittu@gmail.com>
2021-09-25 11:04:13 +0100

    Changed a typo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Star from "./components/Star";
 const App = () => {
   return (
     <div className="App">
-      <h2>Start Components:</h2>
+      <h2>Star Components:</h2>
       <Star color='indigo' />
       <Star color='magenta' isFilled />
     </div>
